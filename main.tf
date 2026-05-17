396e73838ac42214ff0b83432af4d7247be618db
in3t <106376630+in3t@users.noreply.github.com>
2026-05-17 04:58:37 +0000

    adding terraform files

--- main.tf
+++ main.tf
@@ -1,12 +1,9 @@
-provider "aws" {
-   2   │   region = "ap-south-1"
-   3   │ }
-   4   │ 
-   5   │ resource "aws_vpc" "main-vpc" {
-   6   │   cidr_block       = "10.0.0.0/16"
-   7   │   instance_tenancy = "default"
-   8   │ 
-   9   │   tags = {
-  10   │     Name = "main-vpc"
-  11   │   }
-  12   │ }
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "demo-vpc"
+    Purpose = "Jenkins Demo"
+  }
+}
